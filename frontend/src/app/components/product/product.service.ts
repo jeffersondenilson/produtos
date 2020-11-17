e6fb0265fc09365e40d809720b1b37e06c133b7c
jeffersondenilson <jefersond121212@gmail.com>
2020-11-17 15:59:42 -0300

    primeiro commit

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
 import { Product } from './product.model';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
